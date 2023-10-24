d3a24f337a598ad9a298ef2bbb6575cde7ce2626
Tobi Onipede <onipede89@gmail.com>
2023-10-24 22:10:34 +0100

    implement exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+stores = Store.all
+
+@store1 = stores.find_by(id: 1)
+@store2 = stores.find_by(id: 2)
+
+@store1.name = 'Barrhaven'
+
+@store1.save
