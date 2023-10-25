234741597bf1dc94121abaf9f9d9c70c39e3e686
Tobi Onipede <onipede89@gmail.com>
2023-10-25 04:26:03 +0100

    implement validations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 160)
+@store1.employees.create(first_name: "Ijaodala", last_name: "Ayetoro", hourly_rate: 140)
+@store1.employees.create(first_name: "Kate", last_name: "Cabalong", hourly_rate: 145)
+
+
+@store2.employees.create(first_name: "Ojolape", last_name: "Grace", hourly_rate: 80)
+@store2.employees.create(first_name: "Arojo", last_name: "Ayodejo", hourly_rate: 160)
+@store2.employees.create(first_name: "Mathew", last_name: "Kunle", hourly_rate: 140)
+@store2.employees.create(first_name: "Henshaw", last_name: "Harvey", hourly_rate: 60)
