3c72c2708c949c3839a9855aedbf73f789897c15
Tobi Onipede <onipede89@gmail.com>
2023-10-25 04:24:50 +0100

    error

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Welcome, Please enter a store name."
+
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
+store.errors.full_messages.each do |message|
+  puts message
+end
